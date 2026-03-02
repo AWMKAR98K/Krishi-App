39a7a24aaa02eeeae0ff8eb7dd679f81aed9ab01
aumkar <aumkar@gmail.com>
2026-03-03 01:56:20 +0530

    Fix case sensitivity for Bid model

--- backend/routes/yieldRoutes.js
+++ backend/routes/yieldRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Yield = require("../models/Yield");
 const Bid = require("../models/bid");
-const User = require("../models/User");
+const User = require("../models/user");
 const auth = require("../middleware/authMiddleware");
 const upload = require("../middleware/upload");
 
