40a109c447db2f69721c14d46e45fe16d31f8343
aumkar <aumkar@gmail.com>
2026-03-03 10:16:21 +0530

    Fix port binding for Render

--- backend/index.js
+++ backend/index.js
@@ -49,10 +49,10 @@ mongoose.connect(MONGO_URI)
   .then(() => {
     console.log("✅ MongoDB connected successfully");
     // 7. Start Server (ONLY ONCE)
-    const PORT = 5000;
+    const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
-      console.log(`🚀 Server running at http://localhost:5000`);
-    });
+    console.log(`Server is running on port ${PORT}`);
+});
   })
   .catch(err => {
     console.error("❌ MongoDB connection error:", err);
